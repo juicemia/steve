7fb2f226dca6eb600d488e9655fd053543921149
juice <hugo.torres1993@gmail.com>
2020-01-26 23:31:10 -0500

    Add basic implementation of `new` command
    The `new` command is for generating new blog posts. Blog posts are
    backed by Git so the new command is also responsible for creating and
    checking out the branch that the blog post belongs to.

--- cmd/new.go
+++ cmd/new.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
 func init() {
@@ -19,8 +22,42 @@ TODO: update this documentation to give more technical details.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("generating new post...")
 
-		// create git branch
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("error finding current directory: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("opening git repository...")
+		repo, err := git.PlainOpen(wd)
+		if err != nil {
+			fmt.Printf("error opening git repository: %v\n", err)
+			os.Exit(1)
+		}
+
+		worktree, err := repo.Worktree()
+		if err != nil {
+			fmt.Printf("error getting git worktree: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("creating backing branch...")
+		worktree.Checkout(&git.CheckoutOptions{
+			Create: true,
+			Force: false,
+			Branch: plumbing.ReferenceName("refs/heads/post/new-post-test"),
+		})
+		if err != nil {
+			fmt.Printf("error creating new branch for post: %v\n", err)
+			os.Exit(1)
+		}
 
 		// create file
+		fmt.Println("creating markdown file...")
+		_, err = os.Create(fmt.Sprintf("%s/test-blog/blog/new-post-test.md", wd))
+		if err != nil {
+			fmt.Printf("error creating markdown file: %v\n", err)
+			os.Exit(1)
+		}
 	},
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/spf13/cobra v0.0.5
 	gopkg.in/russross/blackfriday.v2 v2.0.0
+	gopkg.in/src-d/go-git.v4 v4.13.1
 )
